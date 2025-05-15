5b37a50dcc39d340a9943e81d1375b5ef72abf86
cooper <pbg0205@naver.com>
2025-05-15 11:33:42 +0900

    first commit

---
+++ src/main/java/kr/mywork/MyworkBeApplication.java
@@ -0,0 +1,13 @@
+package kr.mywork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyworkBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyworkBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/mywork/MyworkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.mywork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyworkBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
