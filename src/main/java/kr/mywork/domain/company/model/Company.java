4e9be7a6828a4bc4f89382f7d9bc096429396928
cooper <pbg0205@naver.com>
2025-05-22 20:29:36 +0900

    feat: Company 및 CompanyId 엔티티 개발(#1)
    - CompanyId
       - 회사 생성 이전에 아이디 발급을 위한 엔티티
       - presigned url 적용을 위한 테이블

---
+++ src/main/java/kr/mywork/domain/company/model/Company.java
@@ -0,0 +1,74 @@
+package kr.mywork.domain.company.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Company {
+
+	@Id
+	@Getter
+	private UUID id;
+
+	@Column(nullable = false, length = 30)
+	private String name;
+
+	@Column(nullable = false, length = 300)
+	private String detail;
+
+	@Column(nullable = false, unique = true, length = 100)
+	private String businessNumber;
+
+	@Column(nullable = false, length = 300)
+	private String address;
+
+	@Column(nullable = false, length = 10)
+	@Enumerated(value = jakarta.persistence.EnumType.STRING)
+	private CompanyType type;
+
+	@Column(nullable = false, length = 50)
+	private String contactPhoneNumber;
+
+	@Column(nullable = false, length = 300)
+	private String contactEmail;
+
+	@Column
+	private String logoImagePath;
+
+	@Column(nullable = false, columnDefinition = "timestamp")
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+
+	@Column(nullable = false, columnDefinition = "timestamp")
+	@UpdateTimestamp
+	private LocalDateTime modifiedAt;
+
+	private Boolean deleted = false;
+
+	public Company(final UUID id, final String name, final String detail, final String businessNumber,
+		final String address, final String type, final String contactPhoneNumber, final String contactEmail,
+		final String logoImagePath) {
+		this.id = id;
+		this.name = name;
+		this.detail = detail;
+		this.businessNumber = businessNumber;
+		this.address = address;
+		this.type = CompanyType.from(type);
+		this.contactPhoneNumber = contactPhoneNumber;
+		this.contactEmail = contactEmail;
+		this.logoImagePath = logoImagePath;
+	}
+
+}
