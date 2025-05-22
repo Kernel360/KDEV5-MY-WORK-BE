85e4a3c1b3ccf383aca005d7ad717023fe0c3b9b
cooper <pbg0205@naver.com>
2025-05-22 20:33:16 +0900

    feat: Company 비즈니스 코드 작성(#1)
    - Company 생성 로직 작성 및 아이디 발급 검증 로직 추가
    - CompanyId 생성 로직 추가

---
+++ src/main/java/kr/mywork/domain/company/service/dto/request/CompanyCreateRequest.java
@@ -0,0 +1,26 @@
+package kr.mywork.domain.company.service.dto.request;
+
+import java.util.UUID;
+
+import kr.mywork.domain.company.model.Company;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CompanyCreateRequest {
+	@Getter
+	private final UUID id;
+	private final String name;
+	private final String detail;
+	private final String businessNumber;
+	private final String address;
+	private final String type;
+	private final String contactPhoneNumber;
+	private final String contactEmail;
+	private final String logoImagePath;
+
+	public Company toEntity() {
+		return new Company(this.id, this.name, this.detail, this.businessNumber, this.address, this.type,
+			this.contactPhoneNumber, this.contactEmail, logoImagePath);
+	}
+}
