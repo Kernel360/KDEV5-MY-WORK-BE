6d240b46db67bce7dee9b7e0319e46e4ee301888
soohalee <suha630@naver.com>
2025-05-25 15:27:32 +0900

    feat: Company 수정 API 레포지토리 코드 작성(#7)

--- src/main/java/kr/mywork/infrastructure/company/rdb/JpaCompanyRepository.java
+++ src/main/java/kr/mywork/infrastructure/company/rdb/JpaCompanyRepository.java
@@ -1,8 +1,9 @@
 package kr.mywork.infrastructure.company.rdb;
 
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.mywork.domain.company.model.Company;
 
-public interface JpaCompanyRepository extends JpaRepository<Company, Long> {
+public interface JpaCompanyRepository extends JpaRepository<Company, UUID> {
 }
--- src/main/java/kr/mywork/infrastructure/company/rdb/QueryDslCompanyRepository.java
+++ src/main/java/kr/mywork/infrastructure/company/rdb/QueryDslCompanyRepository.java
@@ -1,11 +1,13 @@
 package kr.mywork.infrastructure.company.rdb;
 
-import org.springframework.stereotype.Repository;
-
+import java.util.UUID;
+import kr.mywork.domain.company.errors.CompanyErrorType;
+import kr.mywork.domain.company.errors.CompanyNotFoundException;
 import kr.mywork.domain.company.model.Company;
 import kr.mywork.domain.company.repository.CompanyRepository;
 import kr.mywork.domain.company.service.dto.request.CompanyCreateRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,5 +18,13 @@ public class QueryDslCompanyRepository implements CompanyRepository {
 	@Override
 	public Company save(final CompanyCreateRequest companyCreateRequest) {
 		return companyRepository.save(companyCreateRequest.toEntity());
+	}
+
+	@Override
+	public Company findById(UUID id) {
+		Company company = companyRepository.findById(id)
+				.orElseThrow(() -> new CompanyNotFoundException(CompanyErrorType.COMPANY_NOT_FOUND));
+
+		return company;
 	}
 }
