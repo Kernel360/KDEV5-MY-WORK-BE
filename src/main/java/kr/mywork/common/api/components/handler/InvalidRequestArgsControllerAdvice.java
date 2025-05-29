a5d2183abc3791d1052170573478928ee89b41a3
cooper <pbg0205@naver.com>
2025-05-29 20:33:46 +0900

    feat: 회사 목록 조회 컨트롤러 로직 작성 (#15)
    - 요청 값 검증 ControllerAdvice 추가
    - 요청 값 실패 시, status 400 + 에러 필드, 메시지 반환

--- src/main/java/kr/mywork/common/api/components/handler/InvalidRequestArgsControllerAdvice.java
+++ src/main/java/kr/mywork/common/api/components/handler/InvalidRequestArgsControllerAdvice.java
@@ -2,6 +2,10 @@ package kr.mywork.common.api.components.handler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import kr.mywork.common.api.support.error.CommonErrorCode;
 import kr.mywork.common.api.support.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +26,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class InvalidRequestArgsControllerAdvice {
 
 	private static final String ARGUMENT_EXCEPTION_DELIMITER = "/";
+	private static final String FIELD_REGEX = "\\.";
 
 	@ExceptionHandler(value = MethodArgumentNotValidException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -34,6 +39,28 @@ public class InvalidRequestArgsControllerAdvice {
 		final BindingResult bindingResult = methodArgumentNotValidException.getBindingResult();
 		for (FieldError fieldError : bindingResult.getFieldErrors()) {
 			final String errorMessage = String.format("%s (value: %s)", fieldError.getDefaultMessage(), fieldError.getRejectedValue());
+			errorMessages.add(errorMessage);
+		}
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(ApiResponse.error(errorCode.name(), String.join(ARGUMENT_EXCEPTION_DELIMITER, errorMessages), null));
+	}
+
+	@ExceptionHandler(value = ConstraintViolationException .class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ResponseEntity<ApiResponse<?>> handleConstraintViolationException(
+		ConstraintViolationException constraintViolationException) {
+
+		final CommonErrorCode errorCode = CommonErrorCode.ERROR_COMMON02;
+		final List<String> errorMessages = new ArrayList<>();
+
+		final Set<ConstraintViolation<?>> constraintViolations = constraintViolationException.getConstraintViolations();
+		for (ConstraintViolation<?> constraintViolation : constraintViolations) {
+			final String errorMessage = String.format("%s : %s(value: %s)",
+				constraintViolation.getPropertyPath().toString().split(FIELD_REGEX)[1],
+				constraintViolation.getMessage(),
+				constraintViolation.getInvalidValue());
+
 			errorMessages.add(errorMessage);
 		}
 
