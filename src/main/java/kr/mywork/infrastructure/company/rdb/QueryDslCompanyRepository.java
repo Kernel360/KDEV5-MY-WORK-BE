f6c81263c6e08eab8f7fd09d0256baf40c5f76ff
cooper <pbg0205@naver.com>
2025-05-29 20:33:05 +0900

    feat: 회사 목록 조회 레포지토리 로직 추가 (#15)
    - 회사 목록 조회 오프셋 페이징 기반 쿼리 작성
    - 회사 목록 총 갯수 조회 쿼리 작성

--- src/main/java/kr/mywork/infrastructure/company/rdb/QueryDslCompanyRepository.java
+++ src/main/java/kr/mywork/infrastructure/company/rdb/QueryDslCompanyRepository.java
@@ -1,18 +1,31 @@
 package kr.mywork.infrastructure.company.rdb;
 
+import static kr.mywork.domain.company.model.QCompany.company;
+
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
 import kr.mywork.domain.company.model.Company;
 import kr.mywork.domain.company.repository.CompanyRepository;
 import kr.mywork.domain.company.service.dto.request.CompanyCreateRequest;
+import kr.mywork.domain.company.service.dto.response.CompanySelectResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
+@Transactional
 public class QueryDslCompanyRepository implements CompanyRepository {
 
 	private final JpaCompanyRepository companyRepository;
+	private final JPAQueryFactory queryFactory;
 
 	@Override
 	public Company save(final CompanyCreateRequest companyCreateRequest) {
@@ -22,5 +35,55 @@ public class QueryDslCompanyRepository implements CompanyRepository {
 	@Override
 	public Optional<Company> findById(UUID companyId) {
 		return companyRepository.findById(companyId);
+	}
+
+	@Override
+	public List<CompanySelectResponse> findCompaniesBySearchConditionWithPaging(final int page, final int companyPageSize,
+		final String companyType, String keyword, Boolean deleted) {
+
+		final int offset = (page - 1) * companyPageSize;
+
+		return queryFactory.select(Projections.constructor(CompanySelectResponse.class,
+				company.name,
+				company.businessNumber,
+				company.address,
+				company.contactPhoneNumber,
+				company.deleted,
+				company.createdAt))
+			.from(company)
+			.where(
+				company.type.stringValue().eq(companyType),
+				eqDeleted(deleted),
+				eqKeyword(keyword))
+			.offset(offset)
+			.limit(companyPageSize)
+			.fetch();
+	}
+
+	@Override
+	public Long countTotalCompaniesByCondition(final String companyType, final String keyword, final Boolean deleted) {
+		return queryFactory.select(company.id.count())
+			.from(company)
+			.where(
+				company.type.stringValue().eq(companyType),
+				eqDeleted(deleted),
+				eqKeyword(keyword))
+			.fetchOne();
+	}
+
+	private BooleanExpression eqDeleted(Boolean deleted) {
+		if (deleted == null) {
+			return null;
+		}
+
+		return company.deleted.eq(deleted);
+	}
+
+	private BooleanExpression eqKeyword(String keyword) {
+		if (keyword == null) {
+			return null;
+		}
+
+		return company.name.like(keyword + "%");
 	}
 }
