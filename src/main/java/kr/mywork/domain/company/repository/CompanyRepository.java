1b9f35e80e183e09ec34e213e686e13eb4936df7
cooper <pbg0205@naver.com>
2025-05-29 20:32:49 +0900

    feat: 회사 목록 조회 서비스 로직 작성 (#15)
    - 페이지 환경 변수 배치
    - 회사 목록 조회 메서드 추가
    - 회사 목록 총 갯수 조회 메서드 추가

--- src/main/java/kr/mywork/domain/company/repository/CompanyRepository.java
+++ src/main/java/kr/mywork/domain/company/repository/CompanyRepository.java
@@ -1,11 +1,20 @@
 package kr.mywork.domain.company.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 import kr.mywork.domain.company.model.Company;
 import kr.mywork.domain.company.service.dto.request.CompanyCreateRequest;
+import kr.mywork.domain.company.service.dto.response.CompanySelectResponse;
 
 public interface CompanyRepository {
 	Company save(CompanyCreateRequest companyCreateRequest);
+
 	Optional<Company> findById(UUID companyId);
+
+	List<CompanySelectResponse> findCompaniesBySearchConditionWithPaging(int page, int companySize, String companyType,
+		String keyword, Boolean deleted);
+
+	Long countTotalCompaniesByCondition(String companyType, String keyword, Boolean deleted);
 }
