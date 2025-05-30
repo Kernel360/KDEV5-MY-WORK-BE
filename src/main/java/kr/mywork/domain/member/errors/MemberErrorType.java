ecd262cc46f11dfaf41ac72fdb5557bb65659aad
HMHMHMJUN <emmitt9619@gmail.com>
2025-05-31 03:28:58 +0900

    feat: 멤버 생성 에러코드 추가 #38

--- src/main/java/kr/mywork/domain/member/errors/MemberErrorType.java
+++ src/main/java/kr/mywork/domain/member/errors/MemberErrorType.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum MemberErrorType {
-	TYPE_NOT_FOUND(MemberErrorCode.ERROR_MEMBER01, "멤버 타입을 찾을 수 없습니다.");
-
+	TYPE_NOT_FOUND(MemberErrorCode.ERROR_MEMBER01, "멤버 타입을 찾을 수 없습니다."),
+	EMAIL_ALREADY_EXISTS(MemberErrorCode.ERROR_MEMBER02,"이미 가입된 이메일 주소가 있습니다.");
 
 	private final MemberErrorCode errorCode;
 	private final String message;
