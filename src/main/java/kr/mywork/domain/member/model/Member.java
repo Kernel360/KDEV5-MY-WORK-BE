bf753245db814cc7e1ff5b9f9860a0acaed4bd89
Yeju-Kang <bbogaeme@gmail.com>
2025-06-01 21:06:15 +0900

    feat: Member 도메인 및 역할 Enum 정의(#37)

---
+++ src/main/java/kr/mywork/domain/member/model/Member.java
@@ -0,0 +1,49 @@
+package kr.mywork.domain.member.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member {
+
+	@Id
+	private UUID id;
+
+	@Column(name = "company_id", nullable = false)
+	private UUID companyId;
+
+	@Column(length = 30, nullable = false)
+	private String name;
+
+	@Column(length = 100)
+	private String department;
+
+	@Column(length = 50)
+	private String position;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private MemberRole role;
+
+	@Column(length = 300)
+	private String phoneNumber;
+
+	@Column(length = 300, unique = true, nullable = false)
+	private String email;
+
+	@Column(length = 300, nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	private boolean deleted;
+}
