454213c40514c558fa497e2840440b1afae32a14
soohalee <suha630@naver.com>
2025-06-02 16:01:07 +0900

    fix : 게시글 수정 요청시 게시글 id 받는 방법 변경 (#47) - 게시글 id를 받을때 path varialbe 에서 body로 변경합니다.

--- src/main/java/kr/mywork/interfaces/post/controller/dto/request/PostUpdateWebRequest.java
+++ src/main/java/kr/mywork/interfaces/post/controller/dto/request/PostUpdateWebRequest.java
@@ -2,7 +2,6 @@ package kr.mywork.interfaces.post.controller.dto.request;
 
 import java.util.UUID;
 
-import kr.mywork.domain.post.service.dto.request.PostCreateRequest;
 import kr.mywork.domain.post.service.dto.request.PostUpdateRequest;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -11,12 +10,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PostUpdateWebRequest {
 
+	private final UUID id;
+
 	private final String title;
 
 	private final String content;
 
-	public PostUpdateRequest toServiceDto(UUID id) {
-		return new PostUpdateRequest(id, this.title, this.content);
+	public PostUpdateRequest toServiceDto() {
+		return new PostUpdateRequest(this.id, this.title, this.content);
 	}
 
 }
