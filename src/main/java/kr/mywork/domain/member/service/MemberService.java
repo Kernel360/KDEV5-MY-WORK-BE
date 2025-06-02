bea616266d33d75c4267dd0701d3be5e71ea133f
HMHMHMJUN <emmitt9619@gmail.com>
2025-06-02 13:46:03 +0900

    feat: 회사 소속 멤버 조회 서비스 추가 #27

---
+++ src/main/java/kr/mywork/domain/member/service/MemberService.java
@@ -0,0 +1,33 @@
+package kr.mywork.domain.member.service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.mywork.domain.member.repository.MemberRepository;
+import kr.mywork.domain.member.service.dto.response.CompanyMemberResponse;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+	private final MemberRepository memberRepository;
+
+	@Transactional
+	public List<CompanyMemberResponse> findMemberByCompanyId(UUID companyId, Pageable pageable) {
+
+		return memberRepository.findMemberByComapnyId(companyId,pageable).stream()
+			.map(CompanyMemberResponse::fromEntity)
+			.collect(Collectors.toList());
+
+	}
+
+	public long countMembersByCompanyId(UUID companyId) {
+		return memberRepository.countByCompanyIdAndDeletedFalse(companyId);
+	}
+}
