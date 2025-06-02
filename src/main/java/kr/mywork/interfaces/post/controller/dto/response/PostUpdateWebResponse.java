a1816368bac7d5a21f4e29e3d06bf9c5fd85c28a
soohalee <suha630@naver.com>
2025-06-02 16:03:59 +0900

    fix : 게시글 수정 응답 필드 추가 (#47) - 회사이름, 작성자, 승인여부 필드를 추가합니다.

--- src/main/java/kr/mywork/interfaces/post/controller/dto/response/PostUpdateWebResponse.java
+++ src/main/java/kr/mywork/interfaces/post/controller/dto/response/PostUpdateWebResponse.java
@@ -4,10 +4,12 @@ import java.util.UUID;
 
 import kr.mywork.domain.post.service.dto.response.PostUpdateResponse;
 
-public record PostUpdateWebResponse(UUID postId, String title, String content) {
+public record PostUpdateWebResponse(UUID postId, String title, String content, String companyName, String authorName,
+									String approval) {
 
 	public static PostUpdateWebResponse from(PostUpdateResponse postUpdateResponse) {
 		return new PostUpdateWebResponse(postUpdateResponse.id(), postUpdateResponse.title(),
-			postUpdateResponse.content());
+			postUpdateResponse.content(), postUpdateResponse.companyName(), postUpdateResponse.authorName(),
+			postUpdateResponse.approval());
 	}
 }
