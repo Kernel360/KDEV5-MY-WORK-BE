5a6f88c69d45121c45f50d6ef251edee7c6c0e56
HMHMHMJUN <emmitt9619@gmail.com>
2025-06-04 19:01:51 +0900

    [feat] 프로젝트 직원리스트 조회 쿼리 추가 #75

--- src/main/java/kr/mywork/domain/member/repository/MemberRepository.java
+++ src/main/java/kr/mywork/domain/member/repository/MemberRepository.java
@@ -7,16 +7,18 @@ import java.util.UUID;
 import kr.mywork.domain.member.model.Member;
 import kr.mywork.domain.member.service.dto.resquest.MemberCreateRequest;
 
-public interface MemberRepository{
-    Optional<Member> findByEmailAndDeletedFalse(String email);
+public interface MemberRepository {
+	Optional<Member> findByEmailAndDeletedFalse(String email);
 
-	List<Member> findMemberByCompanyId(UUID companyId, int page,int memberPageSize);
+	List<Member> findMemberByCompanyId(UUID companyId, int page, int memberPageSize);
 
 	long countByCompanyIdAndDeletedFalse(UUID companyId);
 
-    Member save(MemberCreateRequest memberCreateRequest);
+	Member save(MemberCreateRequest memberCreateRequest);
 
 	boolean existsByEmail(String email);
 
 	Optional<Member> findById(UUID memberId);
+
+	List<Member> findMemberListByCompanyId(UUID companyId, UUID projectId);
 }
