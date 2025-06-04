e8e6962fb2d78279f2961f3eab186ffee2baa32b
soohalee <suha630@naver.com>
2025-06-04 12:02:01 +0900

    feat : 게시글 삭제 통합테스트 (#55)

---
+++ src/test/resources/sql/post-for-delete.sql
@@ -0,0 +1,25 @@
+INSERT INTO post_id
+VALUES (UNHEX(REPLACE('1234a9a9-90b6-9898-a9dc-92c9861aa98c', '-', '')));
+
+INSERT INTO post (id,
+                  project_step_id,
+                  title,
+                  company_name,
+                  author_name,
+                  content,
+                  approval,
+                  deleted,
+                  modified_at,
+                  created_at
+) VALUES (
+             UNHEX(REPLACE('1234a9a9-90b6-9898-a9dc-92c9861aa98c', '-', '')),
+             UNHEX(REPLACE('4321a2a2-00b2-0000-c2bb-81c0000aa00c', '-', '')),
+             '제목',
+             '회사명',
+             '작성자',
+             '내용',
+             '승인여부',
+             FALSE,
+             NOW(),
+             NOW()
+         );
