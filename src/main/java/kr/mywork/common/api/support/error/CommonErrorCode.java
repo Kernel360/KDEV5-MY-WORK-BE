461617aa12db54ad405ecba032ae990ee88fba91
Yeju-Kang <bbogaeme@gmail.com>
2025-06-04 12:09:23 +0900

    feat: 공통 에러 코드 및 전역 예외 핸들러 구현(#30)
    - 에러 코드 Enum 및 Type 정의
    - GlobalExceptionHandler에서 일관된 예외 응답 처리

--- src/main/java/kr/mywork/common/api/support/error/CommonErrorCode.java
+++ src/main/java/kr/mywork/common/api/support/error/CommonErrorCode.java
@@ -2,6 +2,10 @@ package kr.mywork.common.api.support.error;
 
 public enum CommonErrorCode {
 	ERROR_COMMON01,
-	ERROR_COMMON02
+	ERROR_COMMON02,
+	ERROR_COMMON03,
+	ERROR_COMMON04,
+	ERROR_COMMON05,
+	ERROR_COMMON06;
 	;
 }
--- src/main/java/kr/mywork/common/api/support/error/CommonErrorType.java
+++ src/main/java/kr/mywork/common/api/support/error/CommonErrorType.java
@@ -7,8 +7,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CommonErrorType {
 	ERROR_UNKNOWN(CommonErrorCode.ERROR_COMMON01, "알 수 없는 예외가 발생했습니다."),
-	INVALID_REQUEST_ARGUMENT(CommonErrorCode.ERROR_COMMON02, "유효하지 않는 입력 값을 포함하고 있습니다.");
-
+	INVALID_REQUEST_ARGUMENT(CommonErrorCode.ERROR_COMMON02, "유효하지 않는 입력 값을 포함하고 있습니다."),
+	UNAUTHORIZED(CommonErrorCode.ERROR_COMMON03, "인증이 필요하거나 로그인 정보가 올바르지 않습니다."),
+	INVALID_CONTENT_TYPE(CommonErrorCode.ERROR_COMMON04, "요청의 Content-Type이 application/json이 아닙니다."),
+	INVALID_INPUT_VALUE(CommonErrorCode.ERROR_COMMON05, "이메일 또는 비밀번호가 비어 있습니다."),
+	INVALID_JSON_INPUT(CommonErrorCode.ERROR_COMMON06, "요청의 JSON 파싱에 실패했습니다.");
 	private final CommonErrorCode errorCode;
 	private final String message;
 }
