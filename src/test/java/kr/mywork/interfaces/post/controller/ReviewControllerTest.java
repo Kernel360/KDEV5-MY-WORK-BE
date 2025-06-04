09ea1c75b657abdb47f09c0c74c49fc5a89ac376
cooper <pbg0205@naver.com>
2025-06-04 10:07:39 +0900

    test: 리뷰 수정 컨트롤러 테스트 (#64)
    - 리뷰 수정 값 유효성 검사 테스트

--- src/test/java/kr/mywork/interfaces/post/controller/ReviewControllerTest.java
+++ src/test/java/kr/mywork/interfaces/post/controller/ReviewControllerTest.java
@@ -1,16 +1,22 @@
 package kr.mywork.interfaces.post.controller;
 
+import static org.junit.jupiter.params.provider.Arguments.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
@@ -23,7 +29,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.mywork.common.api.support.response.ResultType;
 import kr.mywork.domain.post.service.ReviewService;
 import kr.mywork.domain.post.service.dto.response.ReviewCreateResponse;
+import kr.mywork.domain.post.service.dto.response.ReviewModifyResponse;
 import kr.mywork.interfaces.post.controller.dto.request.ReviewCreateWebRequest;
+import kr.mywork.interfaces.post.controller.dto.request.ReviewModifyWebRequest;
 
 @WebMvcTest(ReviewController.class)
 class ReviewControllerTest {
@@ -93,4 +101,69 @@ class ReviewControllerTest {
 			.andDo(print());
 	}
 
+	@Test
+	@DisplayName("리뷰 수정 요청 성공")
+	void 리뷰_수정_요청_성공() throws Exception {
+		// given
+		final UUID memberId = UUID.fromString("01973844-b287-73d0-8f9d-f86fad4ac4c3");
+		final UUID reviewId = UUID.fromString("01973844-d55f-7350-b505-f198eaf0cd38");
+
+		when(reviewService.modifyComment(any())).thenReturn(new ReviewModifyResponse(reviewId, "코멘트01_수정"));
+
+		final ReviewModifyWebRequest reviewModifyWebRequest = new ReviewModifyWebRequest("코멘트01_수정");
+
+		final String requestBody = objectMapper.writeValueAsString(reviewModifyWebRequest);
+
+		// when
+		final ResultActions result = mockMvc.perform(put("/api/reviews/{reviewId}", reviewId)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(requestBody));
+
+		// then
+		result.andExpectAll(
+				status().isOk(),
+				jsonPath("$.result").value(ResultType.SUCCESS.name()),
+				jsonPath("$.data").exists(),
+				jsonPath("$.error").doesNotExist())
+			.andDo(print());
+	}
+
+	@ParameterizedTest
+	@MethodSource("reviewModifyInvalidCommentSource")
+	@DisplayName("리뷰 수정 요청 입력 값 실패")
+	void 리뷰_수정_요청_입력_값_실패(String comment) throws Exception {
+		// given
+		final UUID memberId = UUID.fromString("01973844-b287-73d0-8f9d-f86fad4ac4c3");
+		final UUID reviewId = UUID.fromString("01973844-d55f-7350-b505-f198eaf0cd38");
+		final String emptyComment = "  ";
+
+		when(reviewService.modifyComment(any())).thenReturn(new ReviewModifyResponse(reviewId, "코멘트01_수정"));
+
+		final ReviewModifyWebRequest reviewModifyWebRequest = new ReviewModifyWebRequest(emptyComment);
+
+		final String requestBody = objectMapper.writeValueAsString(reviewModifyWebRequest);
+
+		// when
+		final ResultActions result = mockMvc.perform(put("/api/reviews/{reviewId}", reviewId)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(requestBody));
+
+		// then
+		result.andExpectAll(
+				status().is4xxClientError(),
+				jsonPath("$.result").value(ResultType.ERROR.name()),
+				jsonPath("$.data").doesNotExist(),
+				jsonPath("$.error").exists())
+			.andDo(print());
+	}
+
+	private static Stream<Arguments> reviewModifyInvalidCommentSource() {
+		return Stream.of(
+			arguments(""), // 빈 값
+			arguments("   "), // 띄어쓰기 값
+			arguments(
+				"111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111")
+			// 길이 200 초과 값
+		);
+	}
 }
