2b6c7743b40c3ce3ebb918eddf583332b3398b03
soohalee <suha630@naver.com>
2025-06-05 21:17:09 +0900

    feat : 체크리스트 생성 통합테스트 로직 (#78)

---
+++ src/test/java/kr/mywork/docs/ProjectCheckListDocumentationTest.java
@@ -0,0 +1,78 @@
+package kr.mywork.docs;
+
+import static com.epages.restdocs.apispec.ResourceDocumentation.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.epages.restdocs.apispec.ResourceSnippet;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+
+import kr.mywork.common.api.support.response.ResultType;
+import kr.mywork.interfaces.project_checklist.controller.dto.request.ProjectCheckListCreateWebRequest;
+
+public class ProjectCheckListDocumentationTest extends RestDocsDocumentation {
+
+	@Test
+	@DisplayName("체크리스트 생성 성공")
+	@Sql("classpath:sql/project-check-list-for-create.sql")
+	void 체크리스트_생성_성공() throws Exception {
+		//given
+		final String accessToken = createSystemAccessToken();
+		final UUID clientCompanyId = UUID.fromString("1234a9a9-90b6-9898-a9dc-92c9861aa98c"); // UUID ver7
+		final UUID devCompanyId = UUID.fromString("01973f6c-4b84-70e3-be24-1daf26e5808a"); // UUID ver7
+		final UUID projectStepId = UUID.fromString("01991f58-8a6a-7a18-8cfe-1f2bfa6a5e01"); // UUID ver7
+
+		final ProjectCheckListCreateWebRequest projectCheckListCreateWebRequest = new ProjectCheckListCreateWebRequest(
+			"체크리스트 제목", devCompanyId, clientCompanyId, projectStepId, false);
+
+		final String requestBody = objectMapper.writeValueAsString(projectCheckListCreateWebRequest);
+
+		//when
+		final ResultActions result = mockMvc.perform(
+			post("/api/projects/check-lists")
+				.contentType(MediaType.APPLICATION_JSON)
+				.header(HttpHeaders.AUTHORIZATION, toBearerAuthorizationHeader(accessToken))
+				.content(requestBody));
+
+		//then
+		result.andExpectAll(
+				status().isOk(),
+				jsonPath("$.result").value(ResultType.SUCCESS.name()),
+				jsonPath("$.data").exists(),
+				jsonPath("$.error").doesNotExist())
+			.andDo(MockMvcRestDocumentationWrapper.document("project-check-list-create-success", ProjectCheckListCreateSuccessResource()));
+	}
+
+	private ResourceSnippet ProjectCheckListCreateSuccessResource() {
+		return resource(
+			ResourceSnippetParameters.builder()
+				.tag("ProjectCheckList API")
+				.summary("프로젝트 체크 리스트 생성 API")
+				.description("프로젝트 세부 단계에 대한 체크 리스트를 생성한다.")
+				.requestHeaders(
+					headerWithName(HttpHeaders.CONTENT_TYPE).description("컨텐츠 타입"),
+					headerWithName(HttpHeaders.AUTHORIZATION).description("엑세스 토큰"))
+				.responseFields(
+					fieldWithPath("result").type(JsonFieldType.STRING).description("응답 결과"),
+					fieldWithPath("data.title").type(JsonFieldType.STRING).description("체크리스트 제목"),
+					fieldWithPath("data.devCompanyId").type(JsonFieldType.STRING).description("개발사 id"),
+					fieldWithPath("data.clientCompanyId").type(JsonFieldType.STRING).description("고객사 id"),
+					fieldWithPath("data.projectStepId").type(JsonFieldType.STRING).description("프로젝트 단계 id"),
+					fieldWithPath("data.approval").type(JsonFieldType.BOOLEAN).description("승인여부"),
+					fieldWithPath("error").type(JsonFieldType.NULL).description("에러 정보"))
+				.build()
+		);
+	}
+}
