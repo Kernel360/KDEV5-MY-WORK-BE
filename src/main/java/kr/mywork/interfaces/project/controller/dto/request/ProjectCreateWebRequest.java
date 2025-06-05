c030648a2d09f3766d34c2760303413e7808d212
cooper <pbg0205@naver.com>
2025-06-05 16:06:48 +0900

    test: 프로젝트 생성 유효성 검증 테스트 추가

--- src/main/java/kr/mywork/interfaces/project/controller/dto/request/ProjectCreateWebRequest.java
+++ src/main/java/kr/mywork/interfaces/project/controller/dto/request/ProjectCreateWebRequest.java
@@ -3,6 +3,8 @@ package kr.mywork.interfaces.project.controller.dto.request;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
 import kr.mywork.domain.project.service.dto.request.ProjectCreateRequest;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +14,18 @@ import lombok.RequiredArgsConstructor;
 @NoArgsConstructor(force = true)
 @RequiredArgsConstructor
 public class ProjectCreateWebRequest {
+	@Length(min = 1, max = 200, message = "{project.invalid-name-length}")
 	private final String name;
+
 	private final LocalDateTime startAt;
 	private final LocalDateTime endAt;
+
+	@Length(min = 1, max = 200, message = "{project.invalid-step-length}")
 	private final String step;
+
+	@Length(min = 1, max = 500, message = "{project.invalid-detail-length}")
 	private final String detail;
+
 	private final UUID devCompanyId;
 	private final UUID clientCompanyId;
 
