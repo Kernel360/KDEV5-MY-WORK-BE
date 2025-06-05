f5fe03943e8cc489d044d8ccdc551303bf0f9a9a
HMHMHMJUN <emmitt9619@gmail.com>
2025-06-05 20:16:42 +0900

    [feat] 프로젝트 멤버 할당 컨트롤러 추가 #72

---
+++ src/main/java/kr/mywork/interfaces/project_member/controller/ProjectMemberController.java
@@ -0,0 +1,35 @@
+package kr.mywork.interfaces.project_member.controller;
+
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.mywork.common.api.support.response.ApiResponse;
+import kr.mywork.domain.project_member.service.ProjectMemberService;
+import kr.mywork.interfaces.project_member.controller.dto.response.ProjectMemberAddWebResponse;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/project-member")
+@RequiredArgsConstructor
+public class ProjectMemberController {
+
+	final ProjectMemberService projectMemberService;
+
+	@PostMapping("/member")
+	public ApiResponse<ProjectMemberAddWebResponse> projectMemberAddWebResponseApiResponse(
+		@RequestParam(name="projectId") UUID projectId,
+		@RequestParam(name="memberId") UUID memberId
+	) {
+
+		final UUID addedMemberId = projectMemberService.addMemberToCompany(projectId,memberId);
+
+		final ProjectMemberAddWebResponse projectMemberAddWebResponse = new ProjectMemberAddWebResponse(addedMemberId);
+
+		return ApiResponse.success(projectMemberAddWebResponse);
+	}
+}
+
