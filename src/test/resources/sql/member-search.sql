e82d86578f4af86708cb764417a34a6ad35b8d8b
HMHMHMJUN <emmitt9619@gmail.com>
2025-06-08 18:00:17 +0900

    fix 멤버조회 테스트코드 필드 추가 #116

--- src/test/resources/sql/member-search.sql
+++ src/test/resources/sql/member-search.sql
@@ -1,3 +1,30 @@
+INSERT INTO company (
+    deleted,
+    created_at,
+    modified_at,
+    id,
+    name,
+    contact_phone_number,
+    business_number,
+    address,
+    contact_email,
+    detail,
+    logo_image_path,
+    type
+) VALUES (
+             0,
+             NOW(),
+             NOW(),
+             UNHEX(REPLACE('6939d8be-1bf2-4f01-9189-12864e38d913', '-', '')),
+             'Test Company',
+             '+82-10-0000-0000',
+             '1234567890',
+             '123 Test Street, Test City',
+             'testcompany@example.com',
+             'Test company for documentation test',
+             NULL,
+             'CLIENT'
+         );
 
 INSERT INTO member (
     id, company_id, name, department, position,
