38179e04fc893c778a37fdc23500c9a2de1a7762
HMHMHMJUN <emmitt9619@gmail.com>
2025-06-08 13:17:05 +0900

    fix : 회원 목록 조회시 ID 값 포함하도록 수정 #112

--- src/main/java/kr/mywork/interfaces/member/controller/dto/response/MemberSelectWebResponse.java
+++ src/main/java/kr/mywork/interfaces/member/controller/dto/response/MemberSelectWebResponse.java
@@ -1,16 +1,19 @@
 package kr.mywork.interfaces.member.controller.dto.response;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import kr.mywork.domain.member.service.dto.response.MemberSelectResponse;
 
-public record MemberSelectWebResponse(String name, String email, String position, String department, String phoneNumber,
+public record MemberSelectWebResponse(UUID id, String name, String email, String position, String department,
+									  String phoneNumber,
 									  Boolean deleted, @JsonFormat(pattern = "yyyy-MM-dd") LocalDateTime createdAt) {
 
 	public static MemberSelectWebResponse from(MemberSelectResponse memberSelectResponse) {
 		return new MemberSelectWebResponse(
+			memberSelectResponse.id(),
 			memberSelectResponse.name(),
 			memberSelectResponse.email(),
 			memberSelectResponse.position(),
