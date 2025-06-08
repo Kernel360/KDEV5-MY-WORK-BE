0d70f918276e10213599122513b4e9e244416bef
HMHMHMJUN <emmitt9619@gmail.com>
2025-06-08 22:40:10 +0900

    fix: 프로젝트 스탭 생성시 projectId 추가 #122

--- src/main/java/kr/mywork/domain/project_step/serivce/dto/request/ProjectStepCreateRequest.java
+++ src/main/java/kr/mywork/domain/project_step/serivce/dto/request/ProjectStepCreateRequest.java
@@ -1,10 +1,12 @@
 package kr.mywork.domain.project_step.serivce.dto.request;
 
+import java.util.UUID;
+
 import kr.mywork.domain.project_step.model.ProjectStep;
 
 public record ProjectStepCreateRequest(String title, Integer orderNum) {
 
-	public ProjectStep toEntity() {
-		return new ProjectStep(this.title, this.orderNum);
+	public ProjectStep toEntity(UUID projectId) {
+		return new ProjectStep(this.title, this.orderNum,projectId);
 	}
 }
