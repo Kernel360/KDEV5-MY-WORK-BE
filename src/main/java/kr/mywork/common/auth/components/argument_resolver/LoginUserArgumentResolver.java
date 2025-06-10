9014ee406cb11747c4bfec8a1f7b14c8c9e4ef98
cooper <pbg0205@naver.com>
2025-06-10 15:16:50 +0900

    feat: 인증 객체 주입 로직 추가 (#154)

---
+++ src/main/java/kr/mywork/common/auth/components/argument_resolver/LoginUserArgumentResolver.java
@@ -0,0 +1,37 @@
+package kr.mywork.common.auth.components.argument_resolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import kr.mywork.common.auth.components.annotation.LoginMember;
+import kr.mywork.common.auth.components.dto.LoginMemberDetail;
+import kr.mywork.domain.auth.dto.MemberDetails;
+
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+	@Override
+	public boolean supportsParameter(final MethodParameter parameter) {
+		return parameter.getParameterAnnotation(LoginMember.class) != null;
+	}
+
+	@Override
+	public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer,
+		final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) throws Exception {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentication == null) {
+			return null;
+		}
+
+		final MemberDetails memberDetails = (MemberDetails)authentication.getPrincipal();
+
+		return new LoginMemberDetail(memberDetails.getId(), memberDetails.getName(), memberDetails.getCompanyId());
+	}
+}
