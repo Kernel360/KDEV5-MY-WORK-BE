df5951d2f7f7df78d7dfa4c85f12ff7302424579
HMHMHMJUN <emmitt9619@gmail.com>
2025-06-10 19:21:56 +0900

    feat: 멤버 비밀번호 변경  에러코드 추가 #160

--- src/main/java/kr/mywork/domain/member/errors/MemberErrorType.java
+++ src/main/java/kr/mywork/domain/member/errors/MemberErrorType.java
@@ -9,7 +9,8 @@ public enum MemberErrorType {
 	TYPE_NOT_FOUND(MemberErrorCode.ERROR_MEMBER01, "멤버 타입을 찾을 수 없습니다."),
 	EMAIL_ALREADY_EXISTS(MemberErrorCode.ERROR_MEMBER02,"이미 가입된 이메일 주소가 있습니다."),
 	ID_NOT_FOUND(MemberErrorCode.ERROR_MEMBER03,"멤버 ID를 찾을 수 없습니다."),
-	MEMBER_NOT_FOUND(MemberErrorCode.ERROR_MEMBER04,"멤버를 찾을 수 없습니다.");
+	MEMBER_NOT_FOUND(MemberErrorCode.ERROR_MEMBER04,"멤버를 찾을 수 없습니다."),
+	WRONG_PASSWORD(MemberErrorCode.ERROR_MEMBER05,"멤버의 비밀번호가 일치하지 않습니다.");
 
 	private final MemberErrorCode errorCode;
 	private final String message;
