c4d60ee5def87c511513f132e0afead6a0cbff53
cooper <pbg0205@naver.com>
2025-06-10 22:18:44 +0900

    refactor: 프로젝트 수정 API 리팩토링 (#162)
    - 요청 바디 필드 projectId 제거
    - ProjectUpdateRequest 타입 record 로 변경

--- src/main/java/kr/mywork/domain/project/service/dto/request/ProjectUpdateRequest.java
+++ src/main/java/kr/mywork/domain/project/service/dto/request/ProjectUpdateRequest.java
@@ -1,27 +1,7 @@
 package kr.mywork.domain.project.service.dto.request;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
-import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public class ProjectUpdateRequest {
-
-	private final UUID id;
-
-	private final String name;
-
-	private final LocalDateTime startAt;
-
-	private final LocalDateTime endAt;
-
-	private final String step;
-
-	private final String detail;
-
-	private final Boolean deleted;
+public record ProjectUpdateRequest(String name, LocalDateTime startAt, LocalDateTime endAt, String step, String detail,
+								   Boolean deleted) {
 }
