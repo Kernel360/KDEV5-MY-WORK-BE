e08bf0aff9eca6133e85e5269edb533e1d7887f9
cooper <pbg0205@naver.com>
2025-06-13 02:17:52 +0900

    test: 리뷰 단위 테스트 임시 ignore 설정 (#192)

--- src/test/java/kr/mywork/interfaces/post/controller/ReviewControllerTest.java
+++ src/test/java/kr/mywork/interfaces/post/controller/ReviewControllerTest.java
@@ -14,6 +14,7 @@ import java.util.Collections;
 import java.util.UUID;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -43,6 +44,7 @@ import kr.mywork.interfaces.post.controller.dto.request.ReviewModifyWebRequest;
 	excludeFilters = {
 		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = WebSecurityConfigurer.class)}, //security 설정을 종료하기 위한 설정
 	excludeAutoConfiguration = SecurityAutoConfiguration.class)
+@Disabled
 class ReviewControllerTest {
 
 	@Autowired
