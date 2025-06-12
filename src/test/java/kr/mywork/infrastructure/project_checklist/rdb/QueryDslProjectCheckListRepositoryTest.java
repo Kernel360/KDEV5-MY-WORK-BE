364bc8a3f3206757a16057fca323ff0302f4e74f
cooper <pbg0205@naver.com>
2025-06-13 00:19:42 +0900

    test: 체크리스트 목록 조회 레포지토리 테스트 작성 (#125)

--- src/test/java/kr/mywork/infrastructure/project_checklist/rdb/QueryDslProjectCheckListRepositoryTest.java
+++ src/test/java/kr/mywork/infrastructure/project_checklist/rdb/QueryDslProjectCheckListRepositoryTest.java
@@ -1,17 +1,23 @@
 package kr.mywork.infrastructure.project_checklist.rdb;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 
 import kr.mywork.base.annotations.RdbRepositoryTest;
 import kr.mywork.domain.project_checklist.repository.ProjectCheckListRepository;
+import kr.mywork.domain.project_checklist.service.dto.response.ProjectCheckListSelectResponse;
 import kr.mywork.domain.project_checklist.service.dto.response.ProjectStepCheckListCountResponse;
 
 @RdbRepositoryTest(basePackages = {
@@ -63,5 +69,26 @@ class QueryDslProjectCheckListRepositoryTest {
 			softly.assertThat(projectStepCheckListTotalCountResponses).extracting("count")
 				.containsExactly(7L, 7L, 7L);
 		});
+	}
+
+	@ParameterizedTest
+	@MethodSource("findAllByProjectIdAndStepIdMethodSource")
+	@DisplayName("프로젝트 단계의 체크리스트를 조회한다")
+	@Sql("classpath:sql/project_checklist_by_step.sql")
+	void 프로젝트_단계의_체크리스트를_조회한다 (final UUID projectId, final UUID projectStepId, final int expected) {
+		// given, when
+		final List<ProjectCheckListSelectResponse> projectCheckListSelectResponses =
+			projectCheckListRepository.findAllByProjectIdAndStepId(projectId, projectStepId);
+
+		// then
+		assertThat(projectCheckListSelectResponses).hasSize(expected);
+	}
+
+	private static Stream<Arguments> findAllByProjectIdAndStepIdMethodSource() {
+		return Stream.of(
+			Arguments.arguments(UUID.fromString("01975d7f-052c-7d8f-819b-9a08f322ead3"), null, 60),
+			Arguments.arguments(UUID.fromString("01975d7f-052c-7d8f-819b-9a08f322ead3"),
+				"01975dbf-6fbe-71b9-8dab-0455e155bbe8", 20)
+		);
 	}
 }
