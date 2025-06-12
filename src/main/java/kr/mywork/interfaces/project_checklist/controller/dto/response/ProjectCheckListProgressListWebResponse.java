d23917950d300e55595fafc6f042ab48b2129002
cooper <pbg0205@naver.com>
2025-06-12 16:40:04 +0900

    feat: 체크리스트 조회 컨트롤러 추가 (#126)

---
+++ src/main/java/kr/mywork/interfaces/project_checklist/controller/dto/response/ProjectCheckListProgressListWebResponse.java
@@ -0,0 +1,9 @@
+package kr.mywork.interfaces.project_checklist.controller.dto.response;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record ProjectCheckListProgressListWebResponse(
+	@JsonProperty("projectCheckListProgress") List<ProjectCheckListProgressWebResponse> projectCheckListProgressWebResponses) {
+}
