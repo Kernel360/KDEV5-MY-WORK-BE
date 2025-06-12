47238e16303c5d5bc2c936afd434afc1198007e8
cooper <pbg0205@naver.com>
2025-06-12 16:39:21 +0900

    feat: 체크리스트 진척도 비즈니스 개발 (#126)
    1. projectId 기반 projectStep 목록 추출
    2. projectStepId 목록 기반 진척도 그룹핑

--- src/main/java/kr/mywork/domain/project_checklist/service/ProjectCheckListService.java
+++ src/main/java/kr/mywork/domain/project_checklist/service/ProjectCheckListService.java
@@ -1,14 +1,14 @@
 package kr.mywork.domain.project_checklist.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import kr.mywork.domain.company.errors.CompanyErrorType;
-import kr.mywork.domain.company.errors.CompanyNotFoundException;
-import kr.mywork.domain.company.model.CompanyType;
-import kr.mywork.domain.company.repository.CompanyRepository;
 import kr.mywork.domain.project_checklist.errors.ProjectCheckListErrorType;
 import kr.mywork.domain.project_checklist.errors.ProjectCheckListNotFoundException;
 import kr.mywork.domain.project_checklist.model.ProjectCheckList;
@@ -16,10 +16,13 @@ import kr.mywork.domain.project_checklist.repository.ProjectCheckListRepository;
 import kr.mywork.domain.project_checklist.service.dto.request.ProjectCheckListApprovalRequest;
 import kr.mywork.domain.project_checklist.service.dto.request.ProjectCheckListCreateRequest;
 import kr.mywork.domain.project_checklist.service.dto.request.ProjectCheckListUpdateRequest;
+import kr.mywork.domain.project_checklist.service.dto.response.CheckListProjectStepProgressResponse;
 import kr.mywork.domain.project_checklist.service.dto.response.ProjectCheckListApprovalResponse;
 import kr.mywork.domain.project_checklist.service.dto.response.ProjectCheckListCreateResponse;
 import kr.mywork.domain.project_checklist.service.dto.response.ProjectCheckListDetailResponse;
 import kr.mywork.domain.project_checklist.service.dto.response.ProjectCheckListUpdateResponse;
+import kr.mywork.domain.project_checklist.service.dto.response.ProjectStepCheckListCountResponse;
+import kr.mywork.domain.project_step.model.ProjectStep;
 import kr.mywork.domain.project_step.repository.ProjectStepRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -73,12 +76,57 @@ public class ProjectCheckListService {
 	}
 
 	@Transactional
-	public ProjectCheckListApprovalResponse approvalProjectCheckList(ProjectCheckListApprovalRequest projectCheckListApprovalRequest) {
+	public ProjectCheckListApprovalResponse approvalProjectCheckList(
+		ProjectCheckListApprovalRequest projectCheckListApprovalRequest) {
 		ProjectCheckList projectCheckList = projectCheckListRepository.findById(projectCheckListApprovalRequest.getId())
 			.orElseThrow(
 				() -> new ProjectCheckListNotFoundException(ProjectCheckListErrorType.PROJECT_CHECK_LIST_NOT_FOUND));
 
 		projectCheckList.changeApproval(projectCheckListApprovalRequest);
 		return ProjectCheckListApprovalResponse.from(projectCheckList);
+	}
+
+	@Transactional
+	public List<CheckListProjectStepProgressResponse> getCheckListProgress(final UUID projectId,
+		final String approval) {
+
+		final List<ProjectStep> projectSteps = projectStepRepository.findAllByProjectId(projectId);
+		final Map<UUID, ProjectStep> projectStepMap = projectSteps.stream()
+			.collect(Collectors.toMap(ProjectStep::getId, projectStep -> projectStep));
+
+		final List<ProjectStepCheckListCountResponse> projectStepTotalCountResponses =
+			projectCheckListRepository.findProgressCountGroupByProjectStepIdAndApproval(projectStepMap.keySet(), null);
+		final List<ProjectStepCheckListCountResponse> projectStepApprovalCountResponses =
+			projectCheckListRepository.findProgressCountGroupByProjectStepIdAndApproval(projectStepMap.keySet(),
+				approval);
+
+		return transformProjectStepProgress(projectStepTotalCountResponses, projectStepApprovalCountResponses,
+			projectStepMap);
+	}
+
+	private List<CheckListProjectStepProgressResponse> transformProjectStepProgress(
+		final List<ProjectStepCheckListCountResponse> projectStepTotalCountResponses,
+		final List<ProjectStepCheckListCountResponse> projectStepApprovalCountResponses,
+		final Map<UUID, ProjectStep> projectStepMap) {
+
+		// findProgressCountGroupByProjectStepIdAndApproval 에서 순서를 보장해주므로 해당 코드 정상 동작
+		List<CheckListProjectStepProgressResponse> checkListProjectStepProgressResponses = new ArrayList<>();
+		for (int i = 0; i < projectStepTotalCountResponses.size(); i++) {
+			final ProjectStepCheckListCountResponse projectStepTotalCountResponse =
+				projectStepTotalCountResponses.get(i);
+			final ProjectStepCheckListCountResponse projectStepApprovalCountResponse =
+				projectStepApprovalCountResponses.get(i);
+
+			final UUID projectStepId = projectStepTotalCountResponse.projectStepId();
+			final String projectStepName = projectStepMap.get(projectStepId).getTitle();
+
+			checkListProjectStepProgressResponses.add(new CheckListProjectStepProgressResponse(
+				projectStepId,
+				projectStepName,
+				projectStepTotalCountResponse.count(),
+				projectStepApprovalCountResponse.count()));
+		}
+
+		return checkListProjectStepProgressResponses;
 	}
 }
