9004cb3af065d52cbbc2e0bc45a07d6b5e22e728
HMHMHMJUN <emmitt9619@gmail.com>
2025-06-18 19:01:38 +0900

    fix : userType 으로 작성 된 부분 roleName 변경 영향도 #206

--- src/main/java/kr/mywork/common/auth/components/dto/LoginMemberDetail.java
+++ src/main/java/kr/mywork/common/auth/components/dto/LoginMemberDetail.java
@@ -2,5 +2,5 @@ package kr.mywork.common.auth.components.dto;
 
 import java.util.UUID;
 
-public record LoginMemberDetail(UUID memberId, String memberName, UUID companyId, String companyName,String userType) {
+public record LoginMemberDetail(UUID memberId, String memberName, UUID companyId, String companyName,String roleName) {
 }
