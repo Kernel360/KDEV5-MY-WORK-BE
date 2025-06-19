b31b19053b16b73cd895d47674a994c880ed5f5d
HMHMHMJUN <emmitt9619@gmail.com>
2025-06-20 02:48:15 +0900

    feat: 인기있는 게시글 Top5 컨트롤러 추가 #220

--- src/main/java/kr/mywork/interfaces/dashboard/controller/DashboardController.java
+++ src/main/java/kr/mywork/interfaces/dashboard/controller/DashboardController.java
@@ -1,15 +1,22 @@
 package kr.mywork.interfaces.dashboard.controller;
 
-import kr.mywork.common.api.support.response.ApiResponse;
-import kr.mywork.common.auth.components.annotation.LoginMember;
-import kr.mywork.common.auth.components.dto.LoginMemberDetail;
-import kr.mywork.domain.dashboard.service.DashboardService;
-import kr.mywork.interfaces.dashboard.controller.dto.response.DashboardCountSummaryWebResponse;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import kr.mywork.common.api.support.response.ApiResponse;
+import kr.mywork.common.auth.components.annotation.LoginMember;
+import kr.mywork.common.auth.components.dto.LoginMemberDetail;
+import kr.mywork.domain.dashboard.service.DashboardService;
+import kr.mywork.domain.dashboard.service.dto.response.DashboardPopularProjectsResponse;
+import kr.mywork.domain.project.service.ProjectService;
+import kr.mywork.interfaces.dashboard.controller.dto.response.DashboardCountSummaryWebResponse;
+import kr.mywork.interfaces.dashboard.controller.dto.response.DashboardPopularProjectListWebResponse;
+import kr.mywork.interfaces.dashboard.controller.dto.response.DashboardPopularProjectWebResponse;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DashboardController {
 
 	private final DashboardService dashboardService;
+	private final ProjectService projectService;
 
 	@GetMapping("/totalSummery")
 	public ApiResponse<DashboardCountSummaryWebResponse> getDashboardTotalCount(
@@ -27,6 +35,21 @@ public class DashboardController {
 				loginMemberDetail);
 
 		return ApiResponse.success(dashboardCountSummaryWebResponse);
+
+	}
+	@GetMapping("/popularProjects")
+	public    ApiResponse<DashboardPopularProjectListWebResponse> getPopularProjects(
+		@LoginMember final LoginMemberDetail memberDetail
+	){
+		final List<DashboardPopularProjectsResponse> popularProjects = projectService.getPopularProjects(memberDetail);
+
+		final List<DashboardPopularProjectWebResponse> webResponse = popularProjects.stream()
+			.map(DashboardPopularProjectWebResponse::from)
+			.toList();
+
+		final DashboardPopularProjectListWebResponse response = new DashboardPopularProjectListWebResponse(webResponse);
+
+		return ApiResponse.success(response);
 
 	}
 
