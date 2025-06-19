f36f53ff76823c6d2520d527d721c8dae583e97b
cooper <pbg0205@naver.com>
2025-06-19 23:13:31 +0900

    feat: 파일 업로드 활성화 컨트롤러 추가 (#109)

---
+++ src/main/java/kr/mywork/interfaces/post/controller/dto/request/PostAttachmentActiveWebRequest.java
@@ -0,0 +1,15 @@
+package kr.mywork.interfaces.post.controller.dto.request;
+
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
+public class PostAttachmentActiveWebRequest {
+	private final UUID postAttachmentId;
+	private final Boolean active;
+}
