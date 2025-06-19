db9ce3adba53a147b673404c313f68564419a0ae
cooper <pbg0205@naver.com>
2025-06-19 18:28:17 +0900

    feat: 게시글 파일 업로드를 위한 URL 발급 로직 추가 (#109)

--- src/main/java/kr/mywork/domain/post/model/PostAttachment.java
+++ src/main/java/kr/mywork/domain/post/model/PostAttachment.java
@@ -1,15 +1,19 @@
 package kr.mywork.domain.post.model;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import java.time.LocalDateTime;
-import java.util.UUID;
 import kr.mywork.common.rdb.id.UnixTimeOrderedUuidGeneratedValue;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,6 +21,7 @@ public class PostAttachment {
 
     @Id
     @UnixTimeOrderedUuidGeneratedValue
+    @Getter
     private UUID id;
 
     private UUID postId;
@@ -33,4 +38,27 @@ public class PostAttachment {
     @UpdateTimestamp
     private LocalDateTime modifiedAt;
 
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Boolean active;
+
+    private PostAttachment(final UUID postId, final String path, final Boolean deleted, final Boolean active) {
+        this.postId = postId;
+        this.path = path;
+        this.deleted = deleted;
+        this.active = active;
+    }
+
+    public static PostAttachment inactivePostAttachment(final UUID postId, final String path) {
+        return new PostAttachment(postId, path, false, false);
+    }
+
+    public boolean activate() {
+        this.active = true;
+        return true;
+    }
+
+    public boolean isActive() {
+        return this.active;
+    }
 }
