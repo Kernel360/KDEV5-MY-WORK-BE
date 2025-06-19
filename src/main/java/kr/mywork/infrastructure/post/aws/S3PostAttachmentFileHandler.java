2b963feabbdff955925709e66d7700831d1528ff
cooper <pbg0205@naver.com>
2025-06-19 18:28:44 +0900

    feat: S3 기반 업로드 Presigned-URL 발급 로직 추가(#109)

---
+++ src/main/java/kr/mywork/infrastructure/post/aws/S3PostAttachmentFileHandler.java
@@ -0,0 +1,31 @@
+package kr.mywork.infrastructure.post.aws;
+
+import java.net.URL;
+import java.time.Duration;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.awspring.cloud.s3.S3Template;
+import kr.mywork.domain.post.uploader.PostAttachmentFileHandler;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class S3PostAttachmentFileHandler implements PostAttachmentFileHandler {
+
+	@Value("${post.upload.bucket-name}")
+	private String bucketName;
+
+	@Value("${post.upload.duration}")
+	private Duration presignedDuration;
+
+	private final S3Template s3Template;
+
+	@Override
+	public URL createUploadUrl(final UUID postId, final String fileName) {
+		final String filePath = String.format("%s/%s", postId, fileName);
+		return s3Template.createSignedPutURL(bucketName, filePath, presignedDuration);
+	}
+}
