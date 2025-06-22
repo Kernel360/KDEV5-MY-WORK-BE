89cc1e9de2e5a145af4b4ceb0736ea8b9ba749ae
Yeju-Kang <bbogaeme@gmail.com>
2025-06-23 04:50:37 +0900

    feat: 프로젝트 체크리스트 도메인 및 서비스 요청/응답 DTO 수정(#225)

--- src/main/java/kr/mywork/domain/project_checklist/service/dto/response/ProjectCheckListApprovalResponse.java
+++ src/main/java/kr/mywork/domain/project_checklist/service/dto/response/ProjectCheckListApprovalResponse.java
@@ -5,8 +5,8 @@ import java.util.UUID;
 
 import kr.mywork.domain.project_checklist.model.ProjectCheckList;
 
-public record ProjectCheckListApprovalResponse(UUID id, String title, LocalDateTime createdAt, String approval) {
+public record ProjectCheckListApprovalResponse(UUID id, String title, String content, LocalDateTime createdAt, String approval) {
 	public static ProjectCheckListApprovalResponse from(ProjectCheckList projectCheckList) {
-		return new ProjectCheckListApprovalResponse(projectCheckList.getId(), projectCheckList.getTitle(), projectCheckList.getCreatedAt(), projectCheckList.getApproval());
+		return new ProjectCheckListApprovalResponse(projectCheckList.getId(), projectCheckList.getTitle(), projectCheckList.getContent(), projectCheckList.getCreatedAt(), projectCheckList.getApproval());
 	}
 }
--- src/main/java/kr/mywork/domain/project_checklist/service/dto/response/ProjectCheckListDetailResponse.java
+++ src/main/java/kr/mywork/domain/project_checklist/service/dto/response/ProjectCheckListDetailResponse.java
@@ -2,9 +2,9 @@ package kr.mywork.domain.project_checklist.service.dto.response;
 
 import kr.mywork.domain.project_checklist.model.ProjectCheckList;
 
-public record ProjectCheckListDetailResponse(String title, String approval) {
+public record ProjectCheckListDetailResponse(String title, String content, String approval) {
 
 	public ProjectCheckListDetailResponse(ProjectCheckList projectCheckList) {
-		this(projectCheckList.getTitle(), projectCheckList.getApproval());
+		this(projectCheckList.getTitle(), projectCheckList.getContent(), projectCheckList.getApproval());
 	}
 }
