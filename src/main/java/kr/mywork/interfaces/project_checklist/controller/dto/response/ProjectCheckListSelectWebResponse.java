624373d3cf66d4f01cade75fffe0f4d87235f182
Yeju-Kang <bbogaeme@gmail.com>
2025-06-23 04:51:23 +0900

    feat: 프로젝트 체크리스트 WebRequest/WebResponse DTO 수정(#225)

--- src/main/java/kr/mywork/interfaces/project_checklist/controller/dto/response/ProjectCheckListSelectWebResponse.java
+++ src/main/java/kr/mywork/interfaces/project_checklist/controller/dto/response/ProjectCheckListSelectWebResponse.java
@@ -7,13 +7,14 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import kr.mywork.domain.project_checklist.service.dto.response.ProjectCheckListSelectResponse;
 
 public record ProjectCheckListSelectWebResponse(
-	String checkListName, String approval, String projectStepName,
+	String checkListName, String checkListContent, String approval, String projectStepName,
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime createdAt) {
 
 	public static ProjectCheckListSelectWebResponse fromServiceResponse(
 		ProjectCheckListSelectResponse projectCheckListSelectResponse) {
 		return new ProjectCheckListSelectWebResponse(
 			projectCheckListSelectResponse.checkListName(),
+			projectCheckListSelectResponse.checkListContent(),
 			projectCheckListSelectResponse.approval(),
 			projectCheckListSelectResponse.projectStepName(),
 			projectCheckListSelectResponse.createdAt());
--- src/main/java/kr/mywork/interfaces/project_checklist/controller/dto/response/ProjectCheckListUpdateWebResponse.java
+++ src/main/java/kr/mywork/interfaces/project_checklist/controller/dto/response/ProjectCheckListUpdateWebResponse.java
@@ -7,10 +7,10 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import kr.mywork.domain.project_checklist.service.dto.response.ProjectCheckListUpdateResponse;
 
-public record ProjectCheckListUpdateWebResponse(UUID id, String title,
+public record ProjectCheckListUpdateWebResponse(UUID id, String title, String content,
 												@JsonFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime createdAt,
 												String approval) {
 	public ProjectCheckListUpdateWebResponse(ProjectCheckListUpdateResponse response) {
-		this(response.id(), response.title(), response.createdAt(), response.approval());
+		this(response.id(), response.title(), response.content(), response.createdAt(), response.approval());
 	}
 }
