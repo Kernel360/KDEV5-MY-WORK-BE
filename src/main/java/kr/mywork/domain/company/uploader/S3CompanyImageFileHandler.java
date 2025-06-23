c60854d3dab8b023595a867a57047655794d7947
cooper <pbg0205@naver.com>
2025-06-23 20:56:16 +0900

    feat: 회사 로고 이미지 제거 S3 로직 추가 (#233)

--- src/main/java/kr/mywork/domain/company/uploader/S3CompanyImageFileHandler.java
+++ src/main/java/kr/mywork/domain/company/uploader/S3CompanyImageFileHandler.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Component;
 
 import io.awspring.cloud.s3.S3Template;
 import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.S3Object;
 
 @Component
 @RequiredArgsConstructor
@@ -22,6 +26,8 @@ public class S3CompanyImageFileHandler implements CompanyImageFileHandler {
 
 	private final S3Template s3Template;
 
+	private final S3Client s3Client;
+
 	@Override
 	public URL createUploadUrl(final UUID companyId, final String fileName) {
 		final String filePath = String.format("%s/%s", companyId, fileName);
@@ -32,5 +38,19 @@ public class S3CompanyImageFileHandler implements CompanyImageFileHandler {
 	public URL issueDownloadUrl(final String filePath, final Duration duration) {
 		final String key = filePath.replaceFirst("^/", "");
 		return s3Template.createSignedGetURL(bucketName, key, duration);
+	}
+
+	@Override
+	public void deleteImage(final UUID companyId) {
+		final String prefix = String.format("%s/", companyId);
+		ListObjectsV2Request listRequest = ListObjectsV2Request.builder()
+			.bucket(bucketName)
+			.prefix(prefix)
+			.build();
+
+		ListObjectsV2Response listResponse = s3Client.listObjectsV2(listRequest);
+		for (S3Object s3Object : listResponse.contents()) {
+			s3Client.deleteObject(builder -> builder.bucket(bucketName).key(s3Object.key()));
+		}
 	}
 }
