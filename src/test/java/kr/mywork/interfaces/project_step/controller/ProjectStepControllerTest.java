018a1926dbb7fcb3c0c6ab8f4c556c201772bf21
HMHMHMJUN <emmitt9619@gmail.com>
2025-06-23 12:40:55 +0900

    chore: post 조회를 위한 postService 주입 #218

--- src/test/java/kr/mywork/interfaces/project_step/controller/ProjectStepControllerTest.java
+++ src/test/java/kr/mywork/interfaces/project_step/controller/ProjectStepControllerTest.java
@@ -1,18 +1,14 @@
 package kr.mywork.interfaces.project_step.controller;
 
-import static org.junit.jupiter.params.provider.Arguments.arguments;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Stream;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.mywork.common.api.support.response.ResultType;
+import kr.mywork.domain.post.service.PostService;
+import kr.mywork.domain.project_step.serivce.ProjectStepService;
+import kr.mywork.domain.project_step.serivce.dto.response.ProjectStepUpdateResponse;
+import kr.mywork.interfaces.project_step.dto.request.ProjectStepCreateWebRequest;
+import kr.mywork.interfaces.project_step.dto.request.ProjectStepUpdateWebRequest;
+import kr.mywork.interfaces.project_step.dto.request.ProjectStepsCreateWebRequest;
+import kr.mywork.interfaces.project_step.dto.request.ProjectStepsUpdateWebRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -29,15 +25,18 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
 
-import kr.mywork.common.api.support.response.ResultType;
-import kr.mywork.domain.project_step.serivce.ProjectStepService;
-import kr.mywork.domain.project_step.serivce.dto.response.ProjectStepUpdateResponse;
-import kr.mywork.interfaces.project_step.dto.request.ProjectStepCreateWebRequest;
-import kr.mywork.interfaces.project_step.dto.request.ProjectStepUpdateWebRequest;
-import kr.mywork.interfaces.project_step.dto.request.ProjectStepsCreateWebRequest;
-import kr.mywork.interfaces.project_step.dto.request.ProjectStepsUpdateWebRequest;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = ProjectStepController.class,
 	excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = WebSecurityConfigurer.class)}, //security 설정을 종료하기 위한 설정
@@ -52,6 +51,9 @@ class ProjectStepControllerTest {
 
 	@MockitoBean
 	private ProjectStepService projectStepService;
+
+	@MockitoBean
+	private PostService postService;
 
 	@Test
 	@DisplayName("프로젝트 단계 목록 생성 성공")
