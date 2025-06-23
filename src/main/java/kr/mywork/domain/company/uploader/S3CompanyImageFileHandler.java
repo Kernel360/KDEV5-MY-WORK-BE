99e3389907f7b7df830ebd3e93ee88897241872a
cooper <pbg0205@naver.com>
2025-06-23 19:14:02 +0900

    feat: 회사 이미지 S3 업로드 기능 추가 (#233)

---
+++ src/main/java/kr/mywork/domain/company/uploader/S3CompanyImageFileHandler.java
@@ -0,0 +1,30 @@
+package kr.mywork.domain.company.uploader;
+
+import java.net.URL;
+import java.time.Duration;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.awspring.cloud.s3.S3Template;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class S3CompanyImageFileHandler implements CompanyImageFileHandler {
+
+	@Value("${post.upload.bucket-name}")
+	private String bucketName;
+
+	@Value("${company.upload.duration}")
+	private Duration presignedDuration;
+
+	private final S3Template s3Template;
+
+	@Override
+	public URL createUploadUrl(final UUID companyId, final String fileName) {
+		final String filePath = String.format("%s/%s", companyId, fileName);
+		return s3Template.createSignedPutURL(bucketName, filePath, presignedDuration);
+	}
+}
