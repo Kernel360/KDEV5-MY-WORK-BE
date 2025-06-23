5bb1efc1eaba1ad42ca7c609790598f890945231
cooper <pbg0205@naver.com>
2025-06-23 20:24:06 +0900

    feat: 회사 이미지 다운로드 컨트롤러 추가 (#233)

--- src/main/java/kr/mywork/interfaces/company/controller/CompanyImageController.java
+++ src/main/java/kr/mywork/interfaces/company/controller/CompanyImageController.java
@@ -1,15 +1,21 @@
 package kr.mywork.interfaces.company.controller;
 
+import java.util.UUID;
+
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.mywork.common.api.support.response.ApiResponse;
 import kr.mywork.domain.company.service.CompanyImageService;
+import kr.mywork.domain.company.service.dto.response.CompanyImageDownloadUrlIssueResponse;
 import kr.mywork.domain.company.service.dto.response.CompanyImageUploadUrlIssueResponse;
 import kr.mywork.interfaces.company.controller.dto.request.CompanyImageUploadUrlIssueWebRequest;
+import kr.mywork.interfaces.company.controller.dto.response.CompanyImageDownloadUrlIssueWebResponse;
 import kr.mywork.interfaces.company.controller.dto.response.CompanyUploadUrlIssueWebResponse;
 import lombok.RequiredArgsConstructor;
 
@@ -31,4 +37,16 @@ public class CompanyImageController {
 
 		return ApiResponse.success(CompanyUploadUrlIssueWebResponse.fromServiceDto(companyImageUploadUrlIssueResponse));
 	}
+
+	@GetMapping("/images/download-url")
+	public ApiResponse<CompanyImageDownloadUrlIssueWebResponse> downloadImageUrl(
+		@RequestParam("companyId") UUID companyId) {
+
+		final CompanyImageDownloadUrlIssueResponse companyImageDownloadUrlIssueResponse =
+			companyImageService.downloadCompanyImage(companyId);
+
+		return ApiResponse.success(
+			CompanyImageDownloadUrlIssueWebResponse.fromServiceDto(companyImageDownloadUrlIssueResponse));
+	}
+
 }
