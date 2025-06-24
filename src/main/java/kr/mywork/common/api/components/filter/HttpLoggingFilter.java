279f3e2450330122f5d2022ca44fa6432c583974
cooper <pbg0205@naver.com>
2025-06-24 16:15:24 +0900

    feat: HttpLoggingFilter 작업 (#239)

---
+++ src/main/java/kr/mywork/common/api/components/filter/HttpLoggingFilter.java
@@ -0,0 +1,100 @@
+package kr.mywork.common.api.components.filter;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Enumeration;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.mywork.common.api.components.request.HttpRequestBodyCachedWrapper;
+import kr.mywork.common.api.components.response.HttpResponseBodyCachedWrapper;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HttpLoggingFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+		throws IOException, ServletException {
+
+		HttpRequestBodyCachedWrapper requestWrapper =
+			new HttpRequestBodyCachedWrapper((HttpServletRequest)request);
+		HttpResponseBodyCachedWrapper responseWrapper =
+			new HttpResponseBodyCachedWrapper((HttpServletResponse)response);
+
+		logRequest(requestWrapper);
+
+		chain.doFilter(requestWrapper, responseWrapper);
+
+		logResponse(responseWrapper);
+		writeResponseBody(responseWrapper, response);
+	}
+
+	private void logRequest(HttpRequestBodyCachedWrapper request) throws IOException {
+		String url = request.getRequestURI();
+		String method = request.getMethod();
+		String headers = getRequestHeadersAsString(request);
+		String body = getRequestBodyAsString(request);
+
+		log.info("HTTP Request - URL: {}, Method: {}, Headers: {}, Body: {}", url, method, headers, body);
+	}
+
+	private String getRequestHeadersAsString(HttpServletRequest request) {
+		final StringBuilder headers = new StringBuilder();
+		final Enumeration<String> headerNames = request.getHeaderNames();
+
+		while (headerNames.hasMoreElements()) {
+			String headerName = headerNames.nextElement();
+			String headerValue = request.getHeader(headerName);
+
+			if (headerName.length() >= 50) {
+				headerValue = headerValue.substring(0, 50) + "...";
+			}
+
+			headers.append(headerName).append(": ").append(headerValue).append("; ");
+		}
+		return headers.toString().trim();
+	}
+
+	private String getRequestBodyAsString(final HttpRequestBodyCachedWrapper request) throws IOException {
+		return new String(request.getCachedBody(),
+			request.getCharacterEncoding() != null ? request.getCharacterEncoding() : "UTF-8");
+	}
+
+	private void logResponse(final HttpResponseBodyCachedWrapper response) throws IOException {
+		final int status = response.getStatus();
+		final String headers = getResponseHeadersAsString(response);
+		final String body = new String(response.getCachedBody(),
+			(response.getCharacterEncoding() != null) ? response.getCharacterEncoding() : "UTF-8");
+
+		log.info("HTTP Response - Status: {}, Headers: {}, Body: {}", status, headers, body);
+	}
+
+	private String getResponseHeadersAsString(HttpServletResponse response) {
+		final StringBuilder headers = new StringBuilder();
+		final Collection<String> headerNames = response.getHeaderNames();
+
+		for (String headerName : headerNames) {
+			String headerValue = response.getHeader(headerName);
+
+			if (headerValue.length() >= 50) {
+				headerValue = headerValue.substring(0, 50) + "...";
+			}
+
+			headers.append(headerName).append(": ").append(headerValue).append("; ");
+		}
+		return headers.toString().trim();
+	}
+
+	// 우회한 response body byte 코드 주입해야 하므로 제거 금지!!
+	private void writeResponseBody(HttpResponseBodyCachedWrapper cachedResponse, ServletResponse response)
+		throws IOException {
+		final byte[] responseBody = cachedResponse.getCachedBody();
+		response.getOutputStream().write(responseBody);
+	}
+}
