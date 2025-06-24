b0ba332f04f3c63f4aee7e9e851d8a5f2620d582
HMHMHMJUN <emmitt9619@gmail.com>
2025-06-24 15:58:39 +0900

    chore: 대시보드 매출 통계 개발을 위한 공통 설정 영향도 test sql 수정 #240

--- src/test/resources/sql/project-step-get-with-count.sql
+++ src/test/resources/sql/project-step-get-with-count.sql
@@ -1,6 +1,6 @@
-INSERT INTO project (id, name, start_at, end_at, step, created_at, modified_at, detail, deleted)
+INSERT INTO project (id, name, start_at, end_at, step, created_at, modified_at, detail, deleted,project_amount)
 VALUES
-    (UNHEX(REPLACE('0197893a-ad34-734c-97d7-2e0dd6429247', '-', '')), 'ERP 시스템', NOW(), NOW(), '기획', NOW(), NOW(), 'ERP 프로젝트입니다.', 0);
+    (UNHEX(REPLACE('0197893a-ad34-734c-97d7-2e0dd6429247', '-', '')), 'ERP 시스템', NOW(), NOW(), '기획', NOW(), NOW(), 'ERP 프로젝트입니다.', 0,0);
 
 INSERT INTO project_step (id, project_id, title, order_num, created_at)
 VALUES
