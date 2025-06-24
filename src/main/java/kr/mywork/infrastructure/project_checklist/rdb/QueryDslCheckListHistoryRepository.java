ff434cb2ccec9ea74c17cc1808d6a4dfd34645f5
cooper <pbg0205@naver.com>
2025-06-25 08:12:41 +0900

    feat: 체크리스트 히스토리 목록 조회 쿼리 추가 (#254)

--- src/main/java/kr/mywork/infrastructure/project_checklist/rdb/QueryDslCheckListHistoryRepository.java
+++ src/main/java/kr/mywork/infrastructure/project_checklist/rdb/QueryDslCheckListHistoryRepository.java
@@ -1,9 +1,18 @@
 package kr.mywork.infrastructure.project_checklist.rdb;
 
+import static kr.mywork.domain.project_checklist.model.QCheckListHistory.checkListHistory;
+
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import kr.mywork.domain.project_checklist.model.CheckListHistory;
 import kr.mywork.domain.project_checklist.repository.CheckListHistoryRepository;
+import kr.mywork.domain.project_checklist.service.dto.response.CheckListHistorySelectResponse;
 import lombok.RequiredArgsConstructor;
 
 @Repository
@@ -11,9 +20,25 @@ import lombok.RequiredArgsConstructor;
 public class QueryDslCheckListHistoryRepository implements CheckListHistoryRepository {
 
 	private final JpaCheckListRepository jpaCheckListRepository;
+	private final JPAQueryFactory queryFactory;
 
 	@Override
 	public CheckListHistory save(final CheckListHistory checkListHistory) {
 		return jpaCheckListRepository.save(checkListHistory);
+	}
+
+	@Override
+	public List<CheckListHistorySelectResponse> findAllByCheckListId(final UUID checkListId) {
+		return queryFactory.select(Projections.constructor(CheckListHistorySelectResponse.class,
+				checkListHistory.id,
+				checkListHistory.companyName,
+				checkListHistory.authorName,
+				checkListHistory.content,
+				checkListHistory.approval,
+				checkListHistory.createdAt))
+			.from(checkListHistory)
+			.where(checkListHistory.checkListId.eq(checkListId))
+			.orderBy(checkListHistory.id.desc())
+			.fetch();
 	}
 }
