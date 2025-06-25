538263f0e9f3f26e88e716e41e859ede58239d00
leeesooha <suha630@naver.com>
2025-06-25 16:16:19 +0900

    feat : 게시글 승인 API 테스트 코드 & 문서화 추가 (#241)

--- src/main/java/kr/mywork/interfaces/post/controller/dto/request/PostApprovalWebRequest.java
+++ src/main/java/kr/mywork/interfaces/post/controller/dto/request/PostApprovalWebRequest.java
@@ -1,9 +1,11 @@
 package kr.mywork.interfaces.post.controller.dto.request;
 
 import kr.mywork.domain.post.service.dto.response.PostApprovalRequest;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class PostApprovalWebRequest {
 	private String approvalStatus;
 
