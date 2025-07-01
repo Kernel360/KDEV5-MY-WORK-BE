2253f0c9be8cc1892199fd677dafbd25c012410e
cooper <pbg0205@naver.com>
2025-07-01 19:42:47 +0900

    feat: 게시글 승인 요청값 유효성 검증 추가 (#308)

--- src/main/java/kr/mywork/interfaces/post/controller/dto/request/PostApprovalWebRequest.java
+++ src/main/java/kr/mywork/interfaces/post/controller/dto/request/PostApprovalWebRequest.java
@@ -1,5 +1,6 @@
 package kr.mywork.interfaces.post.controller.dto.request;
 
+import jakarta.validation.constraints.Pattern;
 import kr.mywork.domain.post.service.dto.response.PostApprovalRequest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,6 +8,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class PostApprovalWebRequest {
+
+	private static final String POST_APPROVAL_TYPE = "^(APPROVED|PENDING)?$";
+
+	@Pattern(regexp = POST_APPROVAL_TYPE, message = "{invalid.post-approval-type}")
 	private String approvalStatus;
 
 	public PostApprovalRequest toServiceDto() {
