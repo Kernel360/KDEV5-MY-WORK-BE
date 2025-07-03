fa1dd6d240b07419d1dc6cb8063344e96acf28d2
HMHMHMJUN <emmitt9619@gmail.com>
2025-07-03 14:01:12 +0900

    feat: 프로젝트 상태 변경 api 인터셉터 추가 #323

---
+++ src/main/java/kr/mywork/common/api/components/interceptor/PermissionCheckInterceptor.java
@@ -0,0 +1,68 @@
+package kr.mywork.common.api.components.interceptor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.mywork.common.api.support.response.ApiResponse;
+import kr.mywork.domain.auth.dto.MemberDetails;
+import kr.mywork.domain.member.model.MemberRole;
+import kr.mywork.domain.project.errors.ProjectErrorType;
+import kr.mywork.domain.project.model.ProjectMember;
+import kr.mywork.domain.project_member.service.ProjectMemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+public class PermissionCheckInterceptor implements HandlerInterceptor {
+
+    private final ProjectMemberService projectMemberService;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        MemberDetails memberDetails = (MemberDetails) authentication.getPrincipal();
+        UUID memberId = memberDetails.getId();
+        UUID projectId = UUID.fromString(request.getParameter("projectId"));
+        String memberRole = memberDetails.getAuthorityAsStr();
+
+
+        if(MemberRole.SYSTEM_ADMIN.isSameRoleName(memberRole)){
+            return true;
+        }
+
+        if (!request.getMethod().equalsIgnoreCase("GET")) {
+            Optional<ProjectMember> projectManager = projectMemberService.findProjectManagerByMemberIdAndProjectId(memberId, projectId);
+            if (projectManager.isEmpty()) {
+                writeErrorResponse(response);
+                return false;
+
+            }
+        }
+
+        return true;
+    }
+    private void writeErrorResponse(HttpServletResponse response) throws IOException {
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ApiResponse<?> apiResponse = ApiResponse.error(
+                ProjectErrorType.PROJECT_PERMISSION_NOT_FOUND.getCode().name(),
+                ProjectErrorType.PROJECT_PERMISSION_NOT_FOUND.getMessage()
+        );
+
+        objectMapper.writeValue(response.getWriter(), apiResponse);
+    }
+}
