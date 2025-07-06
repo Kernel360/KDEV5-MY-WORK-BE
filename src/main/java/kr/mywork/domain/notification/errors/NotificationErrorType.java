e2eef5cffc65dd065ba6164da087de685294634a
cooper <pbg0205@naver.com>
2025-07-06 14:39:41 +0900

    feat: SSE Emitter 연결 비즈니스 로직 추가 (#329)

--- src/main/java/kr/mywork/domain/notification/errors/NotificationErrorType.java
+++ src/main/java/kr/mywork/domain/notification/errors/NotificationErrorType.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum NotificationErrorType {
-	NOTIFICATION_NOT_FOUND(NotificationErrorCode.ERROR_NOTIFICATION_TYPE01, "존재하지 않는 알림입니다.");
+	NOTIFICATION_NOT_FOUND(NotificationErrorCode.ERROR_NOTIFICATION_TYPE01, "존재하지 않는 알림입니다."),
+	EMITTER_NOT_FOUND(NotificationErrorCode.ERROR_NOTIFICATION_TYPE02, "사용자의 에미터가 존재하지 않습니다.");
 
 	private final NotificationErrorCode errorCode;
 	private final String message;
