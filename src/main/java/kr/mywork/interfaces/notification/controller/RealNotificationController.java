2efa30c8ae5a7efabe84539a2d2256a9f0545322
cooper <pbg0205@naver.com>
2025-07-06 14:39:46 +0900

    feat: SseEmitter 연결 수립 API 개발 (#329)

---
+++ src/main/java/kr/mywork/interfaces/notification/controller/RealNotificationController.java
@@ -0,0 +1,32 @@
+package kr.mywork.interfaces.notification.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import kr.mywork.common.auth.components.annotation.LoginMember;
+import kr.mywork.common.auth.components.dto.LoginMemberDetail;
+import kr.mywork.domain.notification.service.NotificationService;
+import kr.mywork.domain.notification.service.RealTimeNotificationService;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api")
+public class RealNotificationController {
+
+	private final RealTimeNotificationService realTimeNotificationService;
+	private final NotificationService notificationService;
+
+	@GetMapping( value = "/real-notifications/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public ResponseEntity<SseEmitter> connectForRealTimeNotification(@LoginMember LoginMemberDetail loginMemberDetail) {
+		final SseEmitter sseEmitter = realTimeNotificationService.addSseEmitter(loginMemberDetail.memberId());
+		long count = notificationService.countUnreadNotifications(loginMemberDetail.memberId());
+		realTimeNotificationService.sendNotification(loginMemberDetail.memberId(), "unread-notification-count", count);
+		return ResponseEntity.ok(sseEmitter);
+	}
+
+}
